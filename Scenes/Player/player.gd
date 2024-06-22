71e81fe81ca39d3dba66ce953de2dea3cafd2cdc
Christopher Glenn <topherblair@gmail.com>
2024-06-21 23:41:00 -0700

    6/21/2024 NIGHT

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -3,7 +3,7 @@ extends CharacterBody2D
 # player input/movement are mostly controlled here with stats, combat, ect being handled in another script
 @onready var SPRITE = $AnimatedSprite2D
 const TILE_SIZE : int = 8 # movement tile size
-var player_speed : float = 8.0 # movement speed
+var player_speed : float = 0.13 # movement speed
 var player_state : String = "IDLE" # begins at 'IDLE'
 var is_moving : bool = false # is moving check
 var input_direction # movement vector
@@ -22,7 +22,6 @@ func player_input():
     if Input.is_action_pressed("td_UP"):
         # check the raycast collision and do a 'slide' if the player is at a corner
         if !$RayUL.is_colliding() and !$RayUR.is_colliding():
-            $AnimatedSprite2D.play("walkUp") # play walkUp animation
             input_direction = Vector2.UP # change direction to UP
         elif $RayUL.is_colliding() and !$RayUR.is_colliding():
             input_direction = Vector2.RIGHT # slide to the RIGHT
@@ -31,7 +30,6 @@ func player_input():
     elif Input.is_action_pressed("td_RIGHT"):
         # check the raycast collision and do a 'slide' if the player is at a corner
         if !$RayRU.is_colliding() and !$RayRL.is_colliding():
-            $AnimatedSprite2D.play("walkRight") # play walkRight animation
             input_direction = Vector2.RIGHT # change direction to RIGHT
         elif $RayRU.is_colliding() and !$RayRL.is_colliding():
             input_direction = Vector2.DOWN # slide DOWN
@@ -40,7 +38,6 @@ func player_input():
     elif Input.is_action_pressed("td_DOWN"):
         # check the raycast collision and do a 'slide' if the player is at a corner
         if !$RayDL.is_colliding() and !$RayDR.is_colliding():
-            $AnimatedSprite2D.play("walkDown") # play walkDown animation
             input_direction = Vector2.DOWN # change direction to DOWN
         elif $RayDL.is_colliding() and !$RayDR.is_colliding():
             input_direction = Vector2.RIGHT # slide to the RIGHT
@@ -49,14 +46,13 @@ func player_input():
     elif Input.is_action_pressed("td_LEFT"):
         # check the raycast collision and do a 'slide' if the player is at a corner
         if !$RayLU.is_colliding() and !$RayLL.is_colliding():
-            $AnimatedSprite2D.play("walkLeft") # play walkLeft animation
             input_direction = Vector2.LEFT # change direction to LEFT
         elif $RayLU.is_colliding() and !$RayLL.is_colliding():
             input_direction = Vector2.DOWN # slide DOWN
         elif !$RayLU.is_colliding() and $RayLL.is_colliding():
             input_direction = Vector2.UP # slide UP
     else:
-        $AnimatedSprite2D.stop()
+        player_state = "IDLE" # set player state to IDLE
     # DEBUG
     if Input.is_action_just_pressed("td_END"):
         get_tree().quit() # quit the game
@@ -68,7 +64,7 @@ func player_movement():
         if !is_moving:
             is_moving = true # set is_moving to true
             var tween = create_tween() # create tween
-            tween.tween_property(self, "position", position + input_direction*TILE_SIZE, 0.13)
+            tween.tween_property(self, "position", position + input_direction*TILE_SIZE, player_speed)
             tween.tween_callback(end_movement)
 
 func end_movement():
